ca681e4c2bb51c63377477b2da7c738ab0de1b89
seshan-78 <seshan.r_25ec@kgkite.ac.in>
2026-01-30 15:02:27 +0530

    Update problem_61.py

--- problem_61.py
+++ problem_61.py
@@ -3,7 +3,7 @@
 
 def find_pairs(arr, target):
     pairs = []
-    for i in range(len(arr)):
+    for i in range(i,len(arr)):
         for j in range(i, len(arr)):
             if arr[i] + arr[j] == target:
                 pairs.append((arr[i], arr[j]))
