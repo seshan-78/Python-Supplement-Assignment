45e78e48d3b135ea99de1a4de7eaaf7f80b3156c
seshan-78 <seshan.r_25ec@kgkite.ac.in>
2026-01-30 12:02:14 +0530

    Update problem_21.py

--- problem_21.py
+++ problem_21.py
@@ -4,7 +4,7 @@
 def is_sorted(lst):
     for i in range(len(lst) - 1):
         if lst[i] > lst[i + 1]:
-            return False
+            return False 
     return True
 
 numbers = [1, 2, 3, 4, 5]
