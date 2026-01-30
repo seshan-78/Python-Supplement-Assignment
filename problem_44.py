c9bddf3a069671d85316dfe622cf9b1b118e41f3
seshan-78 <seshan.r_25ec@kgkite.ac.in>
2026-01-30 14:44:26 +0530

    Update problem_44.py

--- problem_44.py
+++ problem_44.py
@@ -4,7 +4,7 @@
 def fibonacci(n):
     fib = [0, 1]
     for i in range(2, n):
-        fib.append(fib[i-1] + fib[i-2])
+        fib.append(fib[i-1] + fib[i-2]) 
     return fib
 
 print(f"First 10 Fibonacci numbers: {fibonacci(10)}")
