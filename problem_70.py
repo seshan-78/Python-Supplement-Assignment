5721dcc72ba7baecd15f62fa16bc97c71b6ead99
seshan-78 <seshan.r_25ec@kgkite.ac.in>
2026-01-30 15:06:38 +0530

    Update problem_70.py

--- problem_70.py
+++ problem_70.py
@@ -5,7 +5,7 @@ def find_primes(n):
     primes = []
     for num in range(2, n):
         is_prime = True
-        for i in range(2, num):
+        for i in range(2, num): 
             if num % i == 0:
                 is_prime = False
                 break
