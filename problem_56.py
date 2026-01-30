6e946f3b69f7ad40b75cbda714f638e470a26b4b
seshan-78 <seshan.r_25ec@kgkite.ac.in>
2026-01-30 14:59:41 +0530

    Update problem_56.py

--- problem_56.py
+++ problem_56.py
@@ -5,7 +5,7 @@ def remove_vowels(text):
     vowels = "aeiouAEIOU"
     result = ""
     for char in text:
-        if char not in vowels:
+        if char not in vowels: 
             result += char
     return result
 
