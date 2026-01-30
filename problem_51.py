42aa23cecead378f430828335703ebb0d8c3cdb3
seshan-78 <seshan.r_25ec@kgkite.ac.in>
2026-01-30 14:56:23 +0530

    Update problem_51.py

--- problem_51.py
+++ problem_51.py
@@ -3,7 +3,7 @@
 
 def reverse_words(sentence):
     words = sentence.split()
-    reversed_words = []
+    reversed_words = [] 
     for word in words:
         reversed_words.append(word[::-1])
     return " ".join(reversed_words)
