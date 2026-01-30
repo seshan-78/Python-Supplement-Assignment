119d6907a65ced9f538bae622476072da8ec2d66
seshan-78 <seshan.r_25ec@kgkite.ac.in>
2026-01-30 14:29:09 +0530

    Update problem_32.py

--- problem_32.py
+++ problem_32.py
@@ -1,7 +1,7 @@
 # Problem 32: Find minimum in a list
 # Find and fix the error
 
-numbers = [45, 12, 78, 34, 89]
+numbers = [45, 12, 78, 34, 89] 
 minimum = numbers[0]
 for num in numbers:
     if num < minimum:
